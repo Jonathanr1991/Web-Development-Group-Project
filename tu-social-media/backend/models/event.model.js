6b41088ba64e250eef14250efecd7da5ab3771e3
camerond85 <37337061+camerond85@users.noreply.github.com>
2020-04-19 13:33:09 -0400

    changed eventImage type temporarily, Image was undefined - see TODO comment

--- tu-social-media/backend/models/event.model.js
+++ tu-social-media/backend/models/event.model.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const eventSchema = new Schema({
-    eventName: {type: String, required: true},
+    eventName: { type: String, required: true },
     eventLocation: { type: String, required: true },
-    eventTime:{ type: String, required: true },
-    eventImage:{ type: Image, default: 'Towson_logo.jpg'},
-    eventDescription:{ type: String, required: false},
-    guests:[{ type: Schema.Types.ObjectId, ref: 'user' }],
+    eventTime: { type: String, required: true },
+    eventImage: { type: String, default: "Towson_logo.jpg" }, //TODO type:Image was undefined - could use string if we base36 encode the image, and store that in db?
+    eventDescription: { type: String, required: false },
+    guests: [{ type: Schema.Types.ObjectId, ref: "user" }],
 });
-const event = mongoose.model('event', eventSchema );
-module.exports = event
+const event = mongoose.model("event", eventSchema);
+module.exports = event;
