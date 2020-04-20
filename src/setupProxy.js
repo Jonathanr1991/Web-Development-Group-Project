5859a3b61fb1f543ca63f4d448fa88488074cad7
camerond85 <37337061+camerond85@users.noreply.github.com>
2020-04-20 09:23:24 -0400

    changing .env proxying etc.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,26 @@
 //used to run locally, apparently doesn't matter in Heroku environment
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 require("dotenv").config();
 
 module.exports = function (app) {
     app.use(
-        proxy("/user", { target: "http://" + process.env.HOST + ":3000/" })
+        createProxyMiddleware("/user", {
+            target: "http://" + process.env.HOST + ":3000/",
+        })
     );
     app.use(
-        proxy("/user/*", { target: "http://" + process.env.HOST + ":3000/" })
+        createProxyMiddleware("/user/*", {
+            target: "http://" + process.env.HOST + ":3000/",
+        })
     );
     app.use(
-        proxy("/event", { target: "http://" + process.env.HOST + ":3000/" })
+        createProxyMiddleware("/event", {
+            target: "http://" + process.env.HOST + ":3000/",
+        })
     );
     app.use(
-        proxy("/event/*", { target: "http://" + process.env.HOST + ":3000/" })
+        createProxyMiddleware("/event/*", {
+            target: "http://" + process.env.HOST + ":3000/",
+        })
     );
 };
