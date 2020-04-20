8f470ac40073d09c75989e2b06fe645071ccd2b3
camerond85 <37337061+camerond85@users.noreply.github.com>
2020-04-20 07:09:41 -0400

    adjustments

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
