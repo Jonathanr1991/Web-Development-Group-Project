79a8975048c6d6a71c6b800671f3c86390df3e20
camerond85 <37337061+camerond85@users.noreply.github.com>
2020-04-20 10:25:21 -0400

    changed/parameterized hostname

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,128 +1,153 @@
-import React, {component, Component } from 'react';
-import axios from 'axios';
-import Logo from "../img/Towson_logo.jpg"
+import React, { component, Component } from "react";
+import axios from "axios";
+import Logo from "../img/Towson_logo.jpg";
 
- 
+require("dotenv").config();
+var hostname = "http://localhost:3000";
 //need to verify passwords match
 
 export default class CreateUser extends Component {
-
-  
-
-      constructor(props) {
+    constructor(props) {
         super(props);
-    
+
         this.onChangeEmail = this.onChangeEmail.bind(this);
         this.onChangeFirstName = this.onChangeFirstName.bind(this);
         this.onChangeLastName = this.onChangeLastName.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
         this.onChangeMajor = this.onChangeMajor.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
-    
+
         this.state = {
-            email: '',
-            password: '',
-            firstName: '',
-            lastName: '',
-            major: '',
-        }
-      }
-    
-      onChangeEmail(e) {
+            email: "",
+            password: "",
+            firstName: "",
+            lastName: "",
+            major: "",
+        };
+    }
+
+    onChangeEmail(e) {
         this.setState({
-          email: e.target.value
-        })
-      }
-      onChangePassword(e) {
+            email: e.target.value,
+        });
+    }
+    onChangePassword(e) {
         this.setState({
-          password: e.target.value
-        })
-      }
-      onChangeFirstName(e) {
+            password: e.target.value,
+        });
+    }
+    onChangeFirstName(e) {
         this.setState({
-          firstName: e.target.value
-        })
-      }
-      onChangeLastName(e) {
+            firstName: e.target.value,
+        });
+    }
+    onChangeLastName(e) {
         this.setState({
-          lastName: e.target.value
-        })
-      }
-      onChangeMajor(e) {
+            lastName: e.target.value,
+        });
+    }
+    onChangeMajor(e) {
         this.setState({
-          major: e.target.value
-        })
-      }
-    
-      onSubmit(e) {
+            major: e.target.value,
+        });
+    }
+
+    onSubmit(e) {
         e.preventDefault();
-    
+
         const user = {
             email: this.state.email,
             password: this.state.password,
             firstName: this.state.firstName,
             lastName: this.state.lastName,
             major: this.state.major,
-        }
-    
-        console.log(user);
-        
-    
-       axios.post('http://localhost:5000/user/add', user)
-         .then(res => {
-            console.log(res.data)
-            alert(res.data)
-            
-         } );
-         
-    
-        this.setState({
-          
-        })
-      }
+        };
 
-     
+        console.log(user);
+
+        axios
+            .post(hostname + "/user/add", user)
+            .then((res) => {
+                console.log(res.data);
+                alert(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
+        this.setState({});
+    }
 
     render() {
-        return(
+        return (
             <div className="container">
-            <div className="row  ">
-            
-            <img className="col-6 mt-5 center-block h-100 " src={Logo}/>
-           
-            
-            <div className="col-6">
-            <h1 className=" mt-5 text-center">Sign Up!</h1>
-            <form onSubmit={this.onSubmit}>
-                 
-                 <div className=" form-inline form-group">
-                 <input placeholder="First Name" type="text" required className="col form-control " value={this.state.firstName} onChange={this.onChangeFirstName} />
-                     
-                     <input  placeholder="Last Name" type="text" required className="col ml-2 form-control move-right"   value={this.state.lastName} onChange={this.onChangeLastName} />
-                     
-                     
-                 </div>
-                     
-                     
-                     <input  placeholder="Email"type="email" required className="form-control form-group" value={this.state.email} onChange={this.onChangeEmail} />
-                     
-                     <input  placeholder="Password" minlength="8" type="password" required className="form-control form-group" value={this.state.password} onChange={this.onChangePassword} />
-                     <input  placeholder="Verify Password" minlength="8" type="password" required className="form-control form-group"  />
-                     
-            
-            <div className="form-group">
-              <button type="submit"   className="btn  gold-color " >Create an Account</button>
-            </div>
-                </form>
-            
-                
-            </div>
-              
-            </div>
-            </div>
-            
+                <div className="row  ">
+                    <img
+                        className="col-6 mt-5 center-block h-100 "
+                        src={Logo}
+                    />
 
+                    <div className="col-6">
+                        <h1 className=" mt-5 text-center">Sign Up!</h1>
+                        <form onSubmit={this.onSubmit}>
+                            <div className=" form-inline form-group">
+                                <input
+                                    placeholder="First Name"
+                                    type="text"
+                                    required
+                                    className="col form-control "
+                                    value={this.state.firstName}
+                                    onChange={this.onChangeFirstName}
+                                />
+
+                                <input
+                                    placeholder="Last Name"
+                                    type="text"
+                                    required
+                                    className="col ml-2 form-control move-right"
+                                    value={this.state.lastName}
+                                    onChange={this.onChangeLastName}
+                                />
+                            </div>
+
+                            <input
+                                placeholder="Email"
+                                type="email"
+                                required
+                                className="form-control form-group"
+                                value={this.state.email}
+                                onChange={this.onChangeEmail}
+                            />
+
+                            <input
+                                placeholder="Password"
+                                minlength="8"
+                                type="password"
+                                required
+                                className="form-control form-group"
+                                value={this.state.password}
+                                onChange={this.onChangePassword}
+                            />
+                            <input
+                                placeholder="Verify Password"
+                                minlength="8"
+                                type="password"
+                                required
+                                className="form-control form-group"
+                            />
+
+                            <div className="form-group">
+                                <button
+                                    type="submit"
+                                    className="btn  gold-color "
+                                >
+                                    Create an Account
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         );
-        
     }
 }
