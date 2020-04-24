aacd034e730570105700e7a5425b1eb9d2ef11f7
camerond85 <37337061+camerond85@users.noreply.github.com>
2020-04-24 07:47:57 -0400

    merged changes from master into Heroku branch

--- src/components/user-feed.component.js
+++ src/components/user-feed.component.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class UserFeed extends Component {
     render() {
-        return <h1>user Feed</h1>;
+        return(
+            <h1>user Feed</h1>
+        );
     }
 }
--- src/components/user-profile.component.js
+++ src/components/user-profile.component.js
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
+
 
 export default class UserProfile extends Component {
     render() {
-        return <h1>user Profile</h1>;
+        return(
+            <h1>user Profile</h1>
+        );
     }
 }
