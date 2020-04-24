766db80e76c55a25bd15aff19d826c4007327c75
jrodriquez <jrodri30@students.towson.edu>
2020-04-24 12:16:54 -0400

    updated server.js fie for group router

--- backend/server.js
+++ backend/server.js
@@ -16,50 +16,50 @@ app.use(express.static(path.join(__dirname, "build")));
 //connects mongo to mongodb atlas
 const uri = process.env.ATLAS_URI;
 mongoose //try to connect to cloud, then try local, then just wait for cloud connection
-    .connect(uri, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-    });
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
 
 var connection = mongoose.connection;
 
 connection.on("error", (err) => {
-    console.log(
-        "Got error connecting MongoDB instance at " + uri + " [" + err + "]"
-    );
-    console.log("Attempting to connect to MongoDB (via Mongoose) locally...");
-    mongoose.connect("mongodb://localhost:27017/c484g3test", {
-        //not sure what the idiomatic way of doing a backup connection is
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-    });
-    connection.on("error", () => {
-        console.log(
-            "mongoose couldn't connect to local server either, exiting"
-        );
-        process.kill(1);
-    });
+  console.log(
+    "Got error connecting MongoDB instance at " + uri + " [" + err + "]"
+  );
+  console.log("Attempting to connect to MongoDB (via Mongoose) locally...");
+  mongoose.connect("mongodb://localhost:27017/c484g3test", {
+    //not sure what the idiomatic way of doing a backup connection is
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+  connection.on("error", () => {
+    console.log("mongoose couldn't connect to local server either, exiting");
+    process.kill(1);
+  });
 });
 
 connection.once("open", () => {
-    console.log("MongoDB database connection established successfully");
+  console.log("MongoDB database connection established successfully");
 });
 
 // adding model files to be able to use them
 const userRouter = require("./routes/user");
 const eventRouter = require("./routes/event");
 const postRouter = require("./routes/post");
+const groupRouter = require("./routes/group");
 
 app.use("/user", userRouter);
 app.use("/event", eventRouter);
 app.use("/post", postRouter);
+app.use("/group", groupRouter);
 
 /*app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/build/index.html"));
 });*/
 
 app.listen(port, () => {
-    console.log("Server is running on port: " + port);
+  console.log("Server is running on port: " + port);
 });
