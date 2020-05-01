4f1379caf314744f6514f94281b731b89830d286
camerond85 <37337061+camerond85@users.noreply.github.com>
2020-05-01 18:19:00 -0400

    merging in from org branch

--- src/App.js
+++ src/App.js
@@ -8,23 +8,6 @@ import Navbar from "./components/navbar.component";
 import UserFeed from "./components/user-feed.component";
 import UserProfile from "./components/user-profile.component";
 import Group from "./components/group-component";
-<<<<<<< HEAD
-import Event from "./components/Event-component.js";
-
-function App() {
-    return (
-        <Router>
-            <div className="container">
-                <Navbar />
-                <Route path="/" exact component={CreateUser} />
-                <Route path="/:userId" exact component={UserFeed} />
-                <Route path="/:userId/profile" exact component={UserProfile} />
-                <Route path="/:userId/profile/group" exact component={Group}/>
-                <Route path="/:userId/profile/event" exact component={Event}/>
-            </div>
-        </Router>
-    );
-=======
 import Event from "./components/event-component";
 
 function App() {
@@ -40,7 +23,6 @@ function App() {
       </div>
     </Router>
   );
->>>>>>> master
 }
 
 export default App;
