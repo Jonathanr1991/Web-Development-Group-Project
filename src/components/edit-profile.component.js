142b1ac32abc61aa82132623a6684c4ae5e7e62a
Kevin Valle <49702694+kvalle7@users.noreply.github.com>
2020-05-04 18:52:52 -0400

    Updating files

--- src/components/edit-profile.component.js
+++ src/components/edit-profile.component.js
@@ -1,89 +1,159 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import axios from "axios";
+import pic from "../img/user-profile-pic/default_profile.jpg";
 export default class EditProfile extends Component {
+<<<<<<< HEAD
     render() {
       if(this.props.data.editProfile && this.props.data.loggedIn) {
+=======
+  constructor(props) {
+    super(props);
+>>>>>>> e22ded259f4d2910a5be971da16fa1fe9919c2bf
 
-        return ( 
-          <div>
-            
-            <center> 
-              <div className="panel panel-default" >
-                <div className="panel-heading" >
-                  <div className="form-group" />
-                  <h4 className="col-md-3"><center>Edit Profile</center></h4>
-                </div>
-              </div></center>
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangeFirstName = this.onChangeFirstName.bind(this);
+    this.onChangeLastName = this.onChangeLastName.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangeVerifyPassword = this.onChangeVerifyPassword.bind(this);
+    this.onChangeMajor = this.onChangeMajor.bind(this);
+    this.onChangeBio = this.onChangeBio.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    //this.firstNameClick = this.firstNameClick.bind(this);
 
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <div className="form-group">
+    this.state = {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      major: "",
+      bio: "",
+    };
+  }
 
-                  </div>
-                </div>
-                <div className="form-group">
-                  <label className="col-md-3">Bio</label>
-                  <div className="col-sm-6">
-                    <textarea rows={3} className="form-control" defaultValue={""} />
-                  </div>
-                </div>
-                <div className="panel-body">
-                  <div className="form-group">
-                    <label className="col-md-3">Major</label>
-                    <div className="col-sm-2">
-                      <input type="text" className="form-control" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <br /> <br />
-              
-                <div className="form-group">
-                  <label className="col-md-3">E-mail Address</label>
-                  <div className="col-sm-2">   
-                    <input type="tel" className="form-control" />
-                  </div>
-                </div>
-                <div className="form-group">
-                  <label className="col-md-3">First Name</label>
-                  <div className="col-sm-4">
-                    <input type="email" className="form-control" />
-                  </div>
-                </div>
-                <div className="form-group">
-                  <label className="col-md-3">Last Name</label>
-                  <div className="col-sm-6">
-                    <textarea rows={3} className="form-control" defaultValue={""} />
-                  </div>
-                </div>
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+  onChangeVerifyPassword(e) {
+    this.setState({
+      verifyPassword: e.target.value,
+    });
+  }
+  onChangeFirstName(e) {
+    this.setState({
+      firstName: e.target.value,
+    });
+  }
+  onChangeLastName(e) {
+    this.setState({
+      lastName: e.target.value,
+    });
+  }
+  onChangeMajor(e) {
+    this.setState({
+      major: e.target.value,
+    });
+  }
+  onChangeBio(e) {
+    this.setState({
+      bio: e.target.value,
+    });
+  }
+  firstNameClick() {
+    document.getElementById("firstName").value = this.state.firstName;
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    
+    this.setState({password: this.props.data.user.password})
+    console.log(this.state);
+    axios
+      .post("/user/update/" + this.props.data.user._id, this.state)
+      .then((res) => {
+        console.log(res.data);
+        alert(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    this.setState({});
+  }
+  render() {
+
+    
+    if (this.props.data.editProfile && this.props.data.loggedIn) {
+      return (
+        <div>
+          <br />
+          <br />
+          <br />
+          <div className="container mt-8 center col-6">
+            <form onSubmit={this.onSubmit}>
+              <div className=" form-inline form-group">
+                <a>
+                  <img
+                    src={pic}
+                    className="rounded-circle mx-auto d-block col-4"
+                  ></img>
+                </a>
+                <br />
+                <br />
+                <br />
+                <input
+                  id="firstName"
+                  type="text"
+                  placeholder="Update First Name"
+                  className="col form-control "
+                  //value={this.props.data.user.firstName}
+                  onChange={this.onChangeFirstName}
+                  //onClick={this.firstNameClick}
+                />
+
+                <input
+                  type="text"
+                  placeholder="Update Last Name"
+                  className="col ml-2 form-control move-right"
+                 // value={this.props.data.user.lastName}
+                  onChange={this.onChangeLastName}
+                />
               </div>
 
-            <br /> <br />
-            <center> 
-              <div className="panel panel-default">
-                <div className="panel-heading">
-                  <h4 className="col-md-3"><center>Security</center></h4>
-                </div>
-              </div></center>
-            <div className="form-group">
-              <label className="col-md-3">Current password</label>
-              <div className="col-sm-4">
-                <input type="password" className="form-control" />
+              <input
+                type="email"
+                className="form-control form-group"
+                placeholder="Update Email"
+                //value={this.props.data.user.email}
+                onChange={this.onChangeEmail}
+              />
+
+              <input
+                type="text"
+                className="form-control form-group"
+                placeholder="Update Major"
+               // value={this.props.data.user.major}
+                onChange={this.onChangeMajor}
+              />
+              <textarea
+                className="form-control form-group"
+                placeholder="Update Bio"
+                //value={this.props.data.user.bio}
+                onChange={this.onChangeBio}
+              />
+
+              <div className="form-group">
+                <button type="submit" className="btn  gold-color ">
+                  Update Account
+                </button>
               </div>
-            </div>
-            <div className="form-group">
-              <label className="col-md-3">New password</label>
-              <div className="col-sm-4">
-                <input type="password" className="form-control" />
-              </div>
-            </div>
-            <br /> <br />
-            
-            <div className="form-group">
-              <div className="col-md-3 col-sm-offset-2">
-                <button type="submit" className="btn btn-primary">Submit</button>
-                <button type="reset" className="btn btn-default">Cancel</button>
-              </div>
+<<<<<<< HEAD
             </div>
         )
           
@@ -93,3 +163,14 @@ export default class EditProfile extends Component {
         )
       }
     };
+=======
+            </form>
+          </div>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+}
+>>>>>>> e22ded259f4d2910a5be971da16fa1fe9919c2bf
